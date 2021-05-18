c8ad097daa1296df0eae5ae3b511b6bf7db6245d
Daria Kulynychenko <kulyndar@gmail.com>
2021-05-18 20:13:15 +0200

    add javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"cz.fel.cvut.openk8s.migration"},{"l":"cz.fel.cvut.openk8s.migration.controller"},{"l":"cz.fel.cvut.openk8s.migration.controller.resources"},{"l":"cz.fel.cvut.openk8s.migration.exception"},{"l":"cz.fel.cvut.openk8s.migration.service"},{"l":"cz.fel.cvut.openk8s.migration.service.migration"}]
